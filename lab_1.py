0e1357b4172b17b0914f485f1352303c2704c944
Vicente A <valcalde@miuandes.cl>
2021-03-22 14:55:51 -0300

    correccion tabla

--- lab_1.py
+++ lab_1.py
@@ -3,12 +3,18 @@ import random
 
 
 
-def table ():
-    grand=[]
-    while  len(grand)<4:
-        row=[random.randint(0,21) for _ in range(5)]
-        grand.append(row)
-    return grand 
+def create_table (n_cards):
+    cards_1=[random.randint(1,n_cards) for _ in range(n_cards)]
+    cards_2=cards_1[:]
+    table= cards_1 + cards_2
+    random.shuffle(table) #first disorder
+    random.shuffle(table) #second disorder 
+    return table 
 
-map_table=table()
-print(map_table)
+def print_table (table):
+    return()
+table=create_table(20)
+print(table)
+print(len(table))
+
+
