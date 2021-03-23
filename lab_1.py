8a91788420b40618ccdbf94bd5113271d7d68711
Vicente A <valcalde@miuandes.cl>
2021-03-22 21:34:14 -0300

    version 3.0 union error correction (final?)

--- lab_1.py
+++ lab_1.py
@@ -39,7 +39,7 @@ def cube(table):
 def print_table (table,cord_1,cord_2): #print table with *
     number_col=" "
     for number in range (len(table[0])):
-        number_col =number_col+'%2s  '% str(number+1)
+        number_col =number_col+' %2s  '% str(number+1) #IMprime los mu,erps de arriba 
     print(number_col)
     print()
     n=65
@@ -47,7 +47,8 @@ def print_table (table,cord_1,cord_2): #print table with *
         printin=str(chr(n)+' ')
         for i in range(len(table[x])):
             val=0
-            if table[cord_1[0]][cord_1[1]]== ' ' or table[cord_2[0]][cord_2[1]]== ' ':
+            
+            if table[cord_1[0]-1][cord_1[1]-1]== ' ' or table[cord_2[0]-1][cord_2[1]-1]== ' ':
                 printin=printin+'  '
             else:
                 if x+1==cord_1[0] and i+1==cord_1[1]:
@@ -58,26 +59,33 @@ def print_table (table,cord_1,cord_2): #print table with *
                     val=1
 
                 if val==0:
-                    printin=printin + (' *  ')
-            n+=1
+                    printin=printin + (' %2s '%'*')
+        n+=1
         print(printin)
     return()
 
 def pairs(cord_1,cord_2,table): #check winner pairs
-    if table[cord_1[0]][cord_1[1]]==table[cord_1[0]][cord_1[1]]:
+    if table[cord_1[0]-1][cord_1[1]-1]==table[cord_2[0]-1][cord_2[1]-1]:
         return True
     return False 
+
+def discount_table(table,cord):
+    x=cord[0]
+    y=cord[1]
+    print(y)
+    table[x-1][y-1]='  '
+    return table 
 
 def card():
     print('enter a coordinate ')
     cord=input(' ')
-    return cord 
+    return cord  #se tienes que aplicar los filtros aun 
 
-def convert(card):
+def convert_coordinates(card):
     t=card[0]
     n_t=ord(t)
-    t=n-t -64
-    return (n_t,card[1])
+    t=n_t -64
+    return (t,int(card[1]))
 
 def dimensions(table,cord):
     x=len(table)
@@ -94,7 +102,52 @@ def print_player(score_1 , score_2 , turn): #print the points of the players and
     return
 
 
+stop=0
+score_1=0
+score_2=0
+repit=0
+n_cards= int(input ('enter a number of cards ot press 0 to exit '))
+if n_cards==0:
+     stop=1
+repit=0
+table=create_table(n_cards)
+turn=1
+while stop!=1:
 
+    
+
+    if repit==0:
+        cord_1=(0,0)
+        cord_2=(0,0)
+        if turn==1:
+         turn=2
+        if turn==2:
+           turn=1
+    repit+=1
+      
+    
+    print_player(score_1 , score_2 , turn)
+    print_table(table,cord_1,cord_2)
+    q_card=card()
+    if repit==1:
+        cord_1=convert_coordinates(q_card)
+    if repit==2:
+        cord_2=convert_coordinates(q_card)
+        print_table(table,cord_1,cord_2)
+        if pairs(cord_1,cord_2,table)==True:
+            table=discount_table(table,cord_1)
+            table=discount_table(table,cord_2)
+            if turn==1:
+                score_1+=1
+            if turn==2:
+                score_2+=1
+        else:
+            repit=0
+        cord_1=(0,0)
+        cord_2=(0,0)
+
+
+    
 
 score_1=0
 score_2=0
